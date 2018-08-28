c517b092dca537e39cb6adbba546b7796301030e
howesdomo <164572429@qq.com>
2018-08-28 15:12:21 +0800

    定义Xamarin.Form接口 IScreenDirection

---
+++ Util.XamariN/IScreenDirection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util.XamariN
+{
+    /// <summary>
+    /// 屏幕方向接口
+    /// </summary>
+    public interface IScreenDirection
+    {
+        /// <summary>
+        /// 屏幕方向根据陀螺仪监控结果改变
+        /// </summary>
+        void Unspecified();
+
+        /// <summary>
+        /// 屏幕方向强制竖屏
+        /// </summary>
+        void ForcePortrait();
+
+        /// <summary>
+        /// 屏幕方向强制横屏
+        /// </summary>
+        void ForceLandscape();
+
+        /// <summary>
+        /// 屏幕方向固定 ( 不根据陀螺仪监控结果改变 )
+        /// </summary>
+        void ForceNosensor();
+    }
+}
