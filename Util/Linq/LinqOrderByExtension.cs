6d85d27122a4ae54121bd2b08d33d5abc708d467
howesdomo <164572429@qq.com>
2019-01-09 18:00:23 +0800

    1 ObjectUtils - 待测试 .net Standard 的各个平台中的可行性 2 LinqOrderByExtension.cs  (1) 已测试OrderByRandom()  (2) 待学习鹤神的 OrderBy 扩展方法

---
+++ Util/Linq/LinqOrderByExtension.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+
+namespace System.Linq
+{
+    public static class LinqOrderByExtension
+    {
+        #region Order By Random - 随机排序
+
+        public static IQueryable<T> OrderByRandom<T>(this IQueryable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+
+        public static IEnumerable<T> OrderByRandom<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+
+        // 用 OrderBy(x => Guid.NewGuid()) 这个方法来实现, 思路更佳简洁稳定
+        // 故不再采用以下的代码进行随机排序
+        //public static IOrderedEnumerable<T> OrderByRandom<T>(this IQueryable<T> args)
+        //{
+        //    List<T> list = args.ToList();
+
+        //    Random rand = new Random();
+
+        //    int len = list.Count();
+        //    for (var i = 0; i < len - 1; i++)
+        //    {
+        //        int index = rand.Next(len);
+        //        T temp = list.ElementAt(index);
+        //        list[index] = list[len - i - 1];
+        //        list[len - i - 1] = temp;
+        //    }
+
+        //    return (IOrderedEnumerable<T>)list;
+        //}
+
+        //public static IOrderedEnumerable<T> OrderByRandom2<T>(this List<T> args)
+        //{
+        //    List<T> list = args;
+
+        //    Random rand = new Random();
+
+        //    int len = list.Count();
+        //    for (var i = 0; i < len - 1; i++)
+        //    {
+        //        int index = rand.Next(len);
+        //        T temp = list.ElementAt(index);
+        //        list[index] = list[len - i - 1];
+        //        list[len - i - 1] = temp;
+        //    }
+
+        //    return (IOrderedEnumerable<T>)list;
+        //}
+
+        #endregion
+
+
+    }
+}
---
+++ Util/ObjectUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class ObjectUtilExtension
+    {
+        public static T DeepCloneByBinary<T>(this T obj)
+        {
+            using (System.IO.MemoryStream objStream = new System.IO.MemoryStream()) // 使用序列化来实现深拷贝
+            {
+                System.Runtime.Serialization.IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                formatter.Serialize(objStream, obj); // 将对象或具有给定根的对象图形序列化为所提供的流。
+                objStream.Seek(0, System.IO.SeekOrigin.Begin); // 将当前流中的位置设置为指定值。
+                return (T)formatter.Deserialize(objStream);
+            }
+        }
+
+        public static T DeepCloneByReflection<T>(this T obj)
+        {
+            if (obj is string || obj.GetType().IsValueType) // 如果是字符串或值类型则直接返回
+            {
+                return obj;
+            }
+
+            object retval = Activator.CreateInstance(obj.GetType());
+            System.Reflection.FieldInfo[] fields = obj.GetType().GetFields(System.Reflection.BindingFlags.Public |
+                                                                            System.Reflection.BindingFlags.NonPublic |
+                                                                            System.Reflection.BindingFlags.Instance |
+                                                                            System.Reflection.BindingFlags.Static);
+            foreach (System.Reflection.FieldInfo field in fields)
+            {
+                try { field.SetValue(retval, DeepCloneByReflection(field.GetValue(obj))); }
+                catch (Exception ex)
+                {
+                    string msg = "ObjectUtilExtension - DeepCloneByBinary\r\n{0}".FormatWith(ex.GetFullInfo());
+                    System.Diagnostics.Debug.WriteLine(msg);
+                }
+            }
+            return (T)retval;
+        }
+    }
+}
