d44b458a06f43a9f9416c9626cd996f042fa7548
howesdomo <164572429@qq.com>
2019-08-26 17:21:45 +0800

    1.为了能上传本项目到 Nuget, Util 程序集名称改为 Util.HowesDOMO; 2.Util的AssemblyInfo 的 打包选项 勾选在构建时生产 Nuget 包, 并填写包信息 3.增加FileUtils / FileInfoExtension 新增方法 获取FilenameWithoutExtension

--- Util.XamariN.AndroiD/DLLVersion.cs
+++ Util.XamariN.AndroiD/DLLVersion.cs
@@ -27,6 +27,7 @@ namespace Util.XamariN.AndroiD
 
             history.Add(new DLLVersion() { Version = "1.0.0.0", EntryTime = new DateTime(2019, 8, 3), Description = "优化 Util.Xamarin.AndroiD 项目，由 .net Standard 项目改成 Xamarin.Android, 项目输出设置为 Library( 生成出 DLL 让安卓项目调用 ) " });
             history.Add(new DLLVersion() { Version = "1.0.0.1", EntryTime = new DateTime(2019, 8, 3), Description = "MyScreen 增加 4 个方法, 设置/取消全屏" });
+            history.Add(new DLLVersion() { Version = "1.0.1.0", EntryTime = new DateTime(2019, 8, 3), Description = "发布本项目到 Nuget" });
 
             return history;
         }
