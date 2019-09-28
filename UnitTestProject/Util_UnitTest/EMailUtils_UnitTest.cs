9d079759104ce5b0ffebd6a4d9868d7a7d557064
howesdomo <164572429@qq.com>
2019-09-28 11:44:44 +0800

    修改单元测试

--- UnitTestProject/Util_UnitTest/EMailUtils_UnitTest.cs
+++ UnitTestProject/Util_UnitTest/EMailUtils_UnitTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject.Util
+namespace UnitTestProject
 {
     class EMailUtils_UnitTest
     {
