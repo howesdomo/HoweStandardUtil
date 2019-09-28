5e2ecebf1b8f13bddba0bc886687f9d9150dc51b
howesdomo <164572429@qq.com>
2019-09-28 09:37:57 +0800

    更换硬盘前提交

---
+++ UnitTestProject/Util/EMailUtils_UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.Util
+{
+    class EMailUtils_UnitTest
+    {
+        [TestMethod]
+
+        public void TestSendEMail()
+        {          
+            string pa = Console.ReadLine(); 
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient();
+
+            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = false;
+            smtp.Host = "smtp.qiye.163.com";
+            smtp.Port = 25;
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = new System.Net.NetworkCredential("howe@enpot.com.cn", pa);
+
+            List<string> receive = new List<string>()
+            {
+                "howe@enpot.com.cn"
+            };
+
+            System.Net.Mail.MailPriority mailPriority = System.Net.Mail.MailPriority.Normal;
+
+            string subject = "Xamarin.Forms 发送邮件测试";
+            string content = $"发送邮件测试{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}";
+            mailPriority = System.Net.Mail.MailPriority.Normal;
+
+            List<string> attachmentPathList = new List<string>();
+            //new Util.EMailUtils().SendEMail
+            //(
+            //    sender: "howe@enpot.com.cn",
+            //    smtp: smtp,
+            //    receiverList: receive,
+            //    subject: subject,
+            //    content: content,
+            //    attachmentPathList: attachmentPathList,
+            //    mailPriority: mailPriority
+            //);
+        }
+    }
+}
