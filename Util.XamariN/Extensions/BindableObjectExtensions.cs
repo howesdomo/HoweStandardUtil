e86f9e1e39c428a24da5978ff90403c13d6d70de
howesdomo <164572429@qq.com>
2020-01-03 17:25:06 +0800

    1 EventArgs{T} 补充注释 2 增加 BindableObjectExtensions.cs 3 更新Util.XamariN的nuget文件

---
+++ Util.XamariN/Extensions/BindableObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Xamarin.Forms
+{
+    /// <summary>
+    /// V 1.0.0 - 2020-1-3 17:08:37
+    /// 首次创建
+    /// </summary>
+    public static class BindableObjectExtensions
+    {
+        public static T GetValue<T>(this BindableObject bindableObject, BindableProperty property)
+        {
+            return (T)bindableObject.GetValue(property);
+        }
+    }
+}
