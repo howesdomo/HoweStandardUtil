f5d808484c809a7ab9db32a4e2d2a3877aa9ca79
HowesDOMO <164572429@qq.com>
2021-03-23 20:28:42 +0800

    Util.XamariN V 1.0.16 增加 IVisitSystemSettingUtils.cs 访问操作系统设置功能页面

---
+++ Util.XamariN/IVisitSystemSettingUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util.XamariN
+{
+    /// <summary>
+    /// V 1.0.0 - 2020-10-30 14:02:05
+    /// 首次创建
+    /// </summary>
+    public interface IVisitSystemSettingUtils
+    {
+        void Goto(string str);
+
+        void GotoWireless();
+
+        void GotoWifi();
+
+        void GotoBluetooth();
+
+        void GotoNFC();
+
+        void GotoDate();
+
+        void GotoGPS();
+    }
+}
